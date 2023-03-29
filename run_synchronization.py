7be075cf7d4478c5c83711623809f3e73d32491e
Dekai21 <dickie_zhu@163.com>
2023-03-29 20:07:36 +0200

    added pretrained-weights option in run_synchronization.py

--- run_synchronization.py
+++ run_synchronization.py
@@ -16,6 +16,7 @@ Script to integrate CARLA and SUMO simulations
 import argparse
 import logging
 import time
+import numpy as np
 
 # ==================================================================================================
 # -- find carla module -----------------------------------------------------------------------------
@@ -37,15 +38,10 @@ except IndexError:
 # -- find traci module -----------------------------------------------------------------------------
 # ==================================================================================================
 
-# if 'SUMO_HOME' in os.environ:
-#     sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
-# else:
-#     sys.exit("please declare environment variable 'SUMO_HOME'")
-
-sys.path.append(os.path.join('C:/Users/ZHD1ABT/Downloads/sumo-1.13.0', 'tools'))    # "traci" folder is here, you need to change this path to your sumo path
-from util_0907 import get_intention_from_vehicle_id, get_intention_vector, GNN, rotation_matrix_back, GNN_mtl, GNN_mtl_gnn
-import numpy as np
-import torch
+if 'SUMO_HOME' in os.environ:
+    sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
 
 # ==================================================================================================
 # -- sumo integration imports ----------------------------------------------------------------------
@@ -106,7 +102,8 @@ class SimulationSynchronization(object):
         self.odometers = {}
         self.odometers_cum = []
         self.i_v = 0
-        self.color_box = [(0,0,0),(255,255,255),(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255),(255,0,255),(192,192,192),(128,128,128),(128,0,0),(128,128,0),(0,128,0),(128,0,128),(0,128,128),(0,0,128)]
+        self.color_box = [(0,0,0),(255,255,255),(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255),(255,0,255),\
+            (192,192,192),(128,128,128),(128,0,0),(128,128,0),(0,128,0),(128,0,128),(0,128,128),(0,0,128)]
         self.num_color = len(self.color_box)
 
 
@@ -146,62 +143,6 @@ class SimulationSynchronization(object):
             if sumo_actor_id in self.sumo2carla_ids:
                 self.to_be_destroyed_ids.append(self.sumo2carla_ids.pop(sumo_actor_id))
 
-        # # 在这里构建sumo_control类似的图, 输入模型, 获取控制信号, 当车进入控制圈之后, 接管其控制任务
-        # v_features = []
-        # tgt_sumo_ids = []
-        # graph_sumo_ids = []
-        # for v_id in list(self.sumo2carla_ids.keys()):
-
-        #     sumo_actor = self.sumo.get_actor(v_id)
-        #     # carla_actor = self.carla.get_actor(self.sumo2carla_ids[v_id])
-        #     # x = sumo_actor.transform.location.x
-        #     # y = sumo_actor.transform.location.y
-        #     x = self.carla.get_actor(self.sumo2carla_ids[v_id]).get_location().x
-        #     y = self.carla.get_actor(self.sumo2carla_ids[v_id]).get_location().y
-        #     z = self.carla.get_actor(self.sumo2carla_ids[v_id]).get_location().z
-        #     if abs(z) > 1:
-        #         continue
-        #     carla_yaw = self.carla.get_actor(self.sumo2carla_ids[v_id]).get_transform().rotation.yaw
-        #     # x += np.sin(np.deg2rad(90 - carla_yaw)) * 2.5
-        #     # y += np.cos(np.deg2rad(90 - carla_yaw)) * 2.5
-        #     x += np.cos(np.deg2rad(carla_yaw)) * 2.0
-        #     y += np.sin(np.deg2rad(carla_yaw)) * 2.0
-        #     # if abs(x) <= 70 or abs(y) <= 70:
-        #     if np.linalg.norm(np.array([x, y])) > 70:
-        #         if v_id not in self.trajs.keys():
-        #             self.trajs[v_id] = []
-        #         continue
-        #     if v_id not in self.trajs.keys():
-        #         continue
-        #     if abs(x) > 70 or abs(y) > 70:
-        #         continue
-        #     graph_sumo_ids.append(v_id)
-        #     if np.linalg.norm(np.array([x, y])) < 20  or (v_id in self.control_record and np.linalg.norm(np.array([x, y])) < 60):
-        #         tgt_sumo_ids.append(v_id)
-        #         self.control_record.append(v_id)
-        #     intention = get_intention_vector(get_intention_from_vehicle_id(v_id))
-        #     # v_feature = np.concatenate((np.array([x, -y]), intention)).reshape(1,5)
-        #     v_feature = np.concatenate((np.array([x, y]), intention)).reshape(1,5)
-        #     v_features.append(v_feature)
-        # if len(v_features) > 0:
-        #     v_features = np.concatenate(v_features, axis=0) # [v, 5]
-        #     n_v = v_features.shape[0]
-        #     edge_indexs = torch.tensor([[x,y] for x in range(n_v) for y in range(n_v)]).T.to(self.device)
-        #     v_features = torch.tensor(v_features).float().to(self.device)
-        #     with torch.no_grad():
-        #         ## 1)
-        #         out = self.model(v_features, edge_indexs).cpu().numpy().reshape(n_v, 30, 2).transpose(0,2,1)   # [v, 2, pred]
-                
-        #         ## 2)
-        #         # _out = self.model(v_features, edge_indexs).cpu().numpy().reshape(n_v, 30, 4)
-        #         # out = _out[:,:,:2].transpose(0,2,1)   # [v, 2, pred]
-        #         # out2 = _out[:,:,2:]   # [v, pred, 2]
-
-        #         # out2 = self.model(v_features, edge_indexs).cpu().numpy().reshape(n_v, 30, 2)
-
-        #         ## 3)
-        #         # out = self.model(v_features, edge_indexs).cpu().numpy().reshape(n_v, 30, 2)   # [v, pred, 2]
-
         # Updating sumo actors in carla.
         for sumo_actor_id in self.sumo2carla_ids:
             carla_actor_id = self.sumo2carla_ids[sumo_actor_id]
@@ -218,90 +159,47 @@ class SimulationSynchronization(object):
                 carla_lights = None
             x, y = carla_actor.get_transform().location.x, carla_actor.get_transform().location.y
             if abs(x) < 60 and abs(y) < 60:
+            # Once the vehicles enter the area less than 60m away from the center, the dynamic control (throttle & steering) starts.
                 self.carla.synchronize_vehicle1(carla_actor_id, carla_transform, sumo_actor.velocity, self.steer_record, carla_lights)
                 if abs(x) < 40 and abs(y) < 40:
                     if carla_actor_id not in self.odometers:
                         self.odometers[carla_actor_id] = [] # skip the first step [0,0]
                     else:
                         self.odometers[carla_actor_id].append([x, y])
-
             else:
+            # If the vehicle is more than 60m away from the center, there is no dynamic control, 
+            # and the position of the vehicle is simply updated by coordinate transform
                 self.carla.synchronize_vehicle(carla_actor_id, carla_transform, sumo_actor.velocity, self.steer_record, carla_lights)
 
-            # if sumo_actor_id in tgt_sumo_ids:
-            #     index = graph_sumo_ids.index(sumo_actor_id) # index in graph
+        # # Odometry calculation
+        # if self.sumo.sumo_time > 241:
+        #     for v in self.odometers:
+        #         odometer = self.odometers[v]
+        #         if len(odometer) == 0:
+        #             continue
+        #         odometer = np.array(odometer)   # [step, 2]
+        #         stepwise_move = odometer[:-1,:] - odometer[1:, :]
+        #         stepwise_move = np.linalg.norm(stepwise_move, axis=-1)
+        #         odometer = stepwise_move.sum()
+        #         self.odometers_cum.append(odometer)
+        #     self.odometers_cum = np.array(self.odometers_cum)
+        #     self.odometers_cum = self.odometers_cum[self.odometers_cum > 0]
+        #     self.odometers_cum = self.odometers_cum.sum()
+        #     print("++++++++++++++++++++++++++++++++++++")
+        #     print("Odometers_cum: ", self.odometers_cum)
+        #     print("++++++++++++++++++++++++++++++++++++")
+        #     exit
 
-            #     #################################
-            #     # 1)
-            #     sumo_actor = self.sumo.get_actor(sumo_actor_id)
-            #     yaw_carla = np.deg2rad(sumo_actor.transform.rotation.yaw - 90)
-            #     yaw_carla = np.deg2rad(carla_actor.get_transform().rotation.yaw)
-            #     rotation_back = rotation_matrix_back(yaw_carla) # [2,2]
-            #     global_delta = (rotation_back @ out[index]).transpose(1,0)  # [pred_len, 2]
-            #     x, y = v_features[index][0].item(), v_features[index][1].item()
-            #     x = x - np.cos(yaw_carla) * 2.0
-            #     y = y - np.sin(yaw_carla) * 2.0
-            #     # x = sumo_actor.transform.location.x
-            #     # y = sumo_actor.transform.location.y
-            #     pos = [x+global_delta[0,0], y+global_delta[0,1]]
-            #     # sumo_actor.transform.location.x = pos[0]
-            #     # sumo_actor.transform.location.y = pos[1]
-            #     carla_transform.location.x = pos[0]
-            #     carla_transform.location.y = pos[1]
-            #     # carla_transform.rotation.yaw = np.rad2deg(yaw_carla)
-            #     self.carla.synchronize_vehicle(carla_actor_id, carla_transform, sumo_actor.velocity, self.steer_record, carla_lights)
-
-            #     # 2)
-            #     # acc, delta = out2[index][1]
-            #     # acc, delta = out2[index][2:5,:].mean(axis=0)
-            #     # self.carla.synchronize_vehicle2(carla_actor_id, acc, delta)
-
-            #     # 3)
-            #     # self.carla.synchronize_vehicle3(carla_actor_id, out[index])
-            #     # self.carla.get_actor(carla_actor_id).get_velocity()
-            #     #################################
-
-            # elif sumo_actor_id in self.control_record:
-            #     # self.carla.synchronize_vehicle2(carla_actor_id, 10, 0)
-            #     self.carla.destroy_actor(carla_actor_id)
-
-            # else:
-            #     x, y = self.carla.get_actor(carla_actor_id).get_location().x, self.carla.get_actor(carla_actor_id).get_location().y
-            #     if 80 > abs(x) > 20 or 80 > abs(y) > 20:
-            #         self.carla.synchronize_vehicle(carla_actor_id, carla_transform, sumo_actor.velocity, self.steer_record, carla_lights)    
-            #     else:
-            #         self.carla.synchronize_vehicle(carla_actor_id, carla_transform, sumo_actor.velocity, self.steer_record, carla_lights)
-
-        if self.sumo.sumo_time > 241:
-        # if self.sumo.sumo_time > 101:
-            for v in self.odometers:
-                odometer = self.odometers[v]
-                if len(odometer) == 0:
-                    continue
-                odometer = np.array(odometer)   # [step, 2]
-                stepwise_move = odometer[:-1,:] - odometer[1:, :]
-                stepwise_move = np.linalg.norm(stepwise_move, axis=-1)
-                odometer = stepwise_move.sum()
-                self.odometers_cum.append(odometer)
-            self.odometers_cum = np.array(self.odometers_cum)
-            self.odometers_cum = self.odometers_cum[self.odometers_cum > 0]
-            self.odometers_cum = self.odometers_cum.sum()
-            print("++++++++++++++++++++++++++++++++++++")
-            print("Odometers_cum: ", self.odometers_cum)
-            print("++++++++++++++++++++++++++++++++++++")
-            exit
-
+        # Dealing with the vehicles which already left the intersection in SUMO but are still stuck in Carla due to collision.
         for v in self.to_be_destroyed_ids:
             carla_actor_id = v
             if carla_actor_id not in self.rest_of_life:
                 self.rest_of_life[carla_actor_id] = 100
-                # self.rest_of_life[carla_actor_id] = 1
             carla_actor = self.carla.get_actor(carla_actor_id)
             x, y = carla_actor.get_transform().location.x, carla_actor.get_transform().location.y
             if abs(x) < 60 and abs(y) < 60 and self.rest_of_life[carla_actor_id] > 0:
                 self.carla.synchronize_vehicle2(carla_actor_id)
                 self.rest_of_life[carla_actor_id] -= 1
-                # carla_actor.set_autopilot()
             else:
                 self.carla.destroy_actor(v)
         
@@ -398,7 +296,7 @@ def synchronization_loop(args):
     Entry point for sumo-carla co-simulation.
     """
     sumo_simulation = SumoSimulation(args.sumo_cfg_file, args.step_length, args.sumo_host,
-                                     args.sumo_port, args.sumo_gui, args.client_order)
+                                     args.sumo_port, args.sumo_gui, args.client_order, pretrained=args.pretrained_weights)
     carla_simulation = CarlaSimulation(args.carla_host, args.carla_port, args.step_length)
 
     synchronization = SimulationSynchronization(sumo_simulation, carla_simulation, args.tls_manager,
@@ -480,6 +378,10 @@ if __name__ == '__main__':
                            help="select traffic light manager (default: none)",
                            default='none')
     argparser.add_argument('--debug', action='store_true', help='enable debug messages')
+    argparser.add_argument('--pretrained-weights',
+                           type=str,
+                           help="path to your pretrained GNN weights (default: '')",
+                           default='')
     arguments = argparser.parse_args()
 
     if arguments.sync_vehicle_all is True:
