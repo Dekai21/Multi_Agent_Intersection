401b7c0d9adfdb0f74c3501a692b210bfa041174
Dekai21 <dickie_zhu@163.com>
2023-04-02 23:08:16 +0200

    cleaned train_gnn.py and added more options for training

--- train_gnn.py
+++ train_gnn.py
@@ -1,61 +1,40 @@
 import argparse
-import copy
-import json
 import os
-import pdb
 import pickle
-import pickle as pkl
-import time
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import torch
-import torch.nn.functional as F
 from torch import nn
-from torch.nn import GRU, LSTM, BatchNorm1d, Linear, ReLU, Sequential
-from torch.nn.functional import smooth_l1_loss
-from torch_geometric.data import Data, Dataset, InMemoryDataset
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import (CGConv, EdgeConv, GATConv, GATv2Conv, GCNConv,
-                                GENConv, GeneralConv, GINEConv, GMMConv)
-from torch_geometric.nn import GraphConv
 from torch_geometric.nn import GraphConv as GNNConv
-from torch_geometric.nn import (PDNConv, RGCNConv, SplineConv, TAGConv,
-                                TransformerConv, global_add_pool,
-                                global_max_pool, global_mean_pool)
-from torch_geometric.transforms import BaseTransform
-from torch_geometric.utils import from_networkx
 from tqdm import tqdm
 
-from utils.feature_utils import get_intention_from_vehicle_id
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-from dataset import CarDataset, CarDataset2, CarDataset4, CarDataset5
+from dataset import CarDataset
 from utils.config import DT, OBS_LEN, PRED_LEN
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 obs_len, pred_len, dt = OBS_LEN, PRED_LEN, DT
 
-# pred_len
-exp_id = "sumo_0913"
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('--train_folder', type=str, help='path to the training set', default='csv/train_pre')
+parser.add_argument('--val_folder', type=str, help='path to the validation set', default='csv/val_pre')
+parser.add_argument('--epoch', type=int, help='number of total training epochs', default=20)
+parser.add_argument('--exp_id', type=str, help='experiment ID', default='sumo')
+parser.add_argument('--batch_size', type=int, help='batch size', default=10)
+args = parser.parse_args()
+
+batch_size = args.batch_size   # 8000
+train_folder = args.train_folder
+val_folder = args.val_folder
+exp_id = args.exp_id
 model_path = f"trained_params/{exp_id}"
 os.makedirs(model_path, exist_ok=True)
 
-parser = argparse.ArgumentParser(description="")
-parser.add_argument('--train_folder', type=str, help='path to the training set', default='csv/train')
-parser.add_argument('--val_folder', type=str, help='path to the validation set', default='csv/val')
-parser.add_argument('--epoch', type=int, help='number of total training epochs', default=2000)
-args = parser.parse_args()
-
-train_folder = args.train_folder
-val_folder = args.val_folder
-
 mlp = False
 collision_penalty = True
-batch_size = 8000   # 8000
-train_dataset = CarDataset5(preprocess_folder=train_folder, mlp=False, mpc_aug=False)
-val_dataset = CarDataset5(preprocess_folder=val_folder, mlp=False, mpc_aug=False)
+
+train_dataset = CarDataset(preprocess_folder=train_folder, mlp=False, mpc_aug=True)
+val_dataset = CarDataset(preprocess_folder=val_folder, mlp=False, mpc_aug=True)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=False)
@@ -121,10 +100,10 @@ def train(model, device, data_loader, optimizer, collision_penalty=False):
 
     Parameters:
     model (nn.Module): Model to be trained.
-    loss_fn (nn.Module): Loss function for training.
     device (torch.Device): Device used for training.
     data_loader (torch.utils.data.DataLoader): Data loader containing all batches.
     optimizer (torch.optim.Optimizer): Optimizer used to update model.
+    collision_penalty: set it to True if you want to use collision penalty.
 
     Returns:
     float: Total loss for epoch.
@@ -140,29 +119,23 @@ def train(model, device, data_loader, optimizer, collision_penalty=False):
     for batch in data_loader:
         batch = batch.to(device)
         optimizer.zero_grad()
-        out = model(batch.x[:,[0,1,4,5,6]], batch.edge_index)
-        out = out.reshape(-1,30,2)  # [v, 30, 2]
-        if not collision_penalty:
-            gt = batch.y.reshape(-1,30,8)[:,:,[4,5]]
-            error = ((gt-out).square().sum(-1) * step_weights).sum(-1)
-            loss = (batch.weights * error).nanmean()
-        else:
-            out = out.permute(0,2,1)    # [v, 2, pred]
-            yaw = batch.x[:,3].detach().cpu().numpy()
-            rotations = torch.stack([rotation_matrix_back(yaw[i])  for i in range(batch.x.shape[0])]).to(out.device)
-            out = torch.bmm(rotations, out).permute(0,2,1)       # [v, pred, 2]
-            out += batch.x[:,[0,1]].unsqueeze(1)
-            gt = batch.y.reshape(-1,30,8)[:,:,[0,1]]
-            error = ((gt-out).square().sum(-1) * step_weights).sum(-1)
-            loss = (batch.weights * error).nanmean()
+        out = model(batch.x[:,[0,1,4,5,6]], batch.edge_index)   # [x, y, v, yaw, intention(3-bit)] -> [x, y, intention]
+        out = out.reshape(-1,30,2)  # [v, pred, 2]
+        out = out.permute(0,2,1)    # [v, 2, pred]
+        yaw = batch.x[:,3].detach().cpu().numpy()
+        rotations = torch.stack([rotation_matrix_back(yaw[i])  for i in range(batch.x.shape[0])]).to(out.device)
+        out = torch.bmm(rotations, out).permute(0,2,1)       # [v, pred, 2]
+        out += batch.x[:,[0,1]].unsqueeze(1)
+        gt = batch.y.reshape(-1, 30, 6)[:,:,[0,1]]  # [x, y, v, yaw, acc, steering]
+        error = ((gt-out).square().sum(-1) * step_weights).sum(-1)
+        loss = (batch.weights * error).nanmean()
             
+        if not collision_penalty:
             mask = (batch.edge_index[0,:] < batch.edge_index[1,:])
             _edge = batch.edge_index[:, mask].T   # [edge',2]
             dist = torch.linalg.norm(out[_edge[:,0]] - out[_edge[:,1]], dim=-1)
             dist = dist_threshold - dist[dist < dist_threshold]
             collision_penalty = dist.square().mean()
-            # collision_penalty = smooth_l1_loss(dist, torch.zeros_like(dist), reduction='sum')
-            # loss += collision_penalty * (loss.item()/collision_penalty.item()/10)
             # print(f"loss: {loss.item()}, collision penalty: {collision_penalty.item()}")
             loss += collision_penalty * 20
 
@@ -177,13 +150,11 @@ def evaluate(model, device, data_loader):
 
     Parameters:
     model (nn.Module): Model to be trained.
-    loss_fn (nn.Module): Loss function for training.
     device (torch.Device): Device used for training.
     data_loader (torch.utils.data.DataLoader): Data loader containing all batches.
-    optimizer (torch.optim.Optimizer): Optimizer used to update model.
 
     Returns:
-    float: Total loss for epoch.
+    list of evaluation metrics (including ADE, FDE, etc.).
     """
     step_weights = torch.ones(30, device=device)
     step_weights[:5] *= 5
@@ -251,67 +222,7 @@ def evaluate(model, device, data_loader):
     return ade.item(), fde.item(), mr, collision_rate, val_losses.item(), collision_penalties.item()
 
 
-# def evaluate(model, device, data_loader):
-#     """ Performs an epoch of model training.
-
-#     Parameters:
-#     model (nn.Module): Model to be trained.
-#     loss_fn (nn.Module): Loss function for training.
-#     device (torch.Device): Device used for training.
-#     data_loader (torch.utils.data.DataLoader): Data loader containing all batches.
-#     optimizer (torch.optim.Optimizer): Optimizer used to update model.
-
-#     Returns:
-#     float: Total loss for epoch.
-#     """
-#     model.eval()
-#     ade, fde = [], []
-#     with torch.no_grad():
-#         for batch in data_loader:
-#             batch = batch.to(device)
-#             out = model(batch.x[:,[0,1,4,5,6]], batch.edge_index)
-#             out = out.reshape(-1,30,2)  # [v, 30, 2]
-#             gt = batch.y.reshape(-1,30,8)[:,:,[4,5]]
-#             error = torch.linalg.norm(gt-out, dim=-1)   # [v, 30]
-#             fde.append(error[:,-1])
-#             ade.append(error.mean(dim=-1))
-#     ade = torch.cat(ade).mean()
-#     fde = torch.cat(fde).mean()
-#     return ade, fde
-
-# losses = list()
-# print(checkpoint_dir)
-# checkpoint_dir = 'D:/codes/tmp/dekai_intellisys_ws2021/gnn_theta/trained_params/sumo_0911/model_rot_gnn_mtl_wp400_sumo_0911_e3_0990.pth'
-# if checkpoint_dir:
-#     # load_checkpoint(checkpoint_dir, model)
-#     checkpoint = torch.load(checkpoint_dir, map_location=torch.device('cpu'))
-#     model.load_state_dict(checkpoint)
-
 model = model.to(device)
-
-# min_ade = 1e6
-# min_fde = 1e6
-# best_epoch = 0
-# patience = 100
-# optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-# for epoch in tqdm(range(0, 80000)):
-#     loss = train(model, device, train_loader, optimizer, collision_penalty)
-#     if epoch % 20 == 0:
-#         ade, fde = evaluate(model, device, val_loader)
-#         print(f"Epoch {epoch}: Train Loss: {loss}, ADE: {ade}, FDE: {fde}, lr: {optimizer.param_groups[0]['lr']}.")
-#         if fde < min_fde:
-#             min_ade, min_fde = ade, fde
-#             best_epoch = epoch
-#             print(f'Epoch {epoch}: save the model.')
-#             torch.save(model.state_dict(), model_path + f"/model_{'mlp' if mlp else 'gnn'}_mtl_{'wp' if collision_penalty else 'np'}_sumo_0910_e3.pth")
-#             # torch.save(model.state_dict(), model_path + f"/model_rot_tx_64_InD_0906.pth")
-#         elif (epoch - best_epoch) > patience:
-#             if patience > 1600: # x16
-#                 print(f"{'MLP' if mlp else 'GNN'} earlier stops, Best Epoch: {best_epoch}, Min ADE: {min_ade}, Min FDE: {min_fde}.")
-#                 break
-#             else:
-#                 optimizer.param_groups[0]['lr'] *= 0.5
-#                 patience *= 2
 
 min_ade = 1e6
 min_fde = 1e6
@@ -324,15 +235,18 @@ for epoch in tqdm(range(0, args.epoch)):
     if epoch % 10 == 0:
         ade, fde, mr, collision_rate, val_losses, collision_penalties = evaluate(model, device, val_loader)
         record.append([ade, fde, mr, collision_rate, val_losses, collision_penalties])
-        print(f"Epoch {epoch}: Train Loss: {loss}, ADE: {ade}, FDE: {fde}, MR: {mr}, CR:{collision_rate}, Val_loss: {val_losses}, CP: {collision_penalties}, lr: {optimizer.param_groups[0]['lr']}.")
-        torch.save(model.state_dict(), model_path + f"/model_{'mlp' if mlp else 'gnn'}_{'wp' if collision_penalty else 'np'}_{exp_id}_e3_{str(epoch).zfill(4)}.pth")
+        print(f"Epoch {epoch}: Train Loss: {loss}, ADE: {ade}, FDE: {fde}, MR: {mr}, CR:{collision_rate}, \
+            Val_loss: {val_losses}, CP: {collision_penalties}, lr: {optimizer.param_groups[0]['lr']}.")
+        torch.save(model.state_dict(), model_path + \
+            f"/model_{'mlp' if mlp else 'gnn'}_{'wp' if collision_penalty else 'np'}_{exp_id}_e3_{str(epoch).zfill(4)}.pth")
         if fde < min_fde:
             min_ade, min_fde = ade, fde
             best_epoch = epoch
             print(" !!! New smallest FDE !!! ")
         elif (epoch - best_epoch) > patience:
             if patience > 1600: # x16
-                print(f"{'MLP' if mlp else 'GNN'} earlier stops, Best Epoch: {best_epoch}, Min ADE: {min_ade}, Min FDE: {min_fde}, MR: {mr}, CR:{collision_rate}.")
+                print(f"{'MLP' if mlp else 'GNN'} earlier stops, Best Epoch: {best_epoch}, Min ADE: {min_ade}, \
+                    Min FDE: {min_fde}, MR: {mr}, CR:{collision_rate}.")
                 break
             else:
                 optimizer.param_groups[0]['lr'] *= 0.5
